182575590a00c1885a09c224a41504de2420aaec
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2025-03-13 00:42:49 +0300

    Add project files.

---
+++ Core/RentCar.Domain/Entities/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int KM { get; set; }
+        //type degeri otomati k mi manuel mi 
+        public string Type { get; set; }
+        //yakit tipi benzin mazot elektrik hybrid
+        public string Fuel { get; set; }
+        public decimal DailyPrice { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Core/RentCar.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        //rentedcar modelinden sonra object kismini degistirecegiz
+        public List<RentedCar> RentedCars { get; set; }
+    }
+}
